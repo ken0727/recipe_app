9e6db2f24cdb9dd2715232ee17fe56174d344a93
ken0727 <ayame0727@gmail.com>
2023-12-05 15:58:39 +0900

    ブックマークから詳細ページへ移動

--- resources/views/bookmark_recipe/index.blade.php
+++ resources/views/bookmark_recipe/index.blade.php
@@ -12,12 +12,14 @@
             @foreach ($bookmarkedRecipes as $recipe)
                 <div class="col-md-4">
                     <div class="post-image-wrapper">
-                    @if ($recipe->post->image_path)
-                        <img src="{{ asset('storage/' . $recipe->post->image_path) }}" alt="{{ $recipe->post->name }}" width="300" height="200">
-                    @else
-                        <img src="{{ asset('storage/Noimage.jpeg') }}" alt="Noimage" width="300" height="200">
-                    @endif
-                    <h3>{{ $recipe->post->name }}</h3>
+                        <a href="{{ route('posts.show', $recipe->post) }}">
+                            @if ($recipe->post->image_path)
+                                <img src="{{ asset('storage/' . $recipe->post->image_path) }}" alt="{{ $recipe->post->name }}" width="300" height="200">
+                            @else
+                                <img src="{{ asset('storage/Noimage.jpeg') }}" alt="Noimage" width="300" height="200">
+                            @endif
+                            <h3>{{ $recipe->post->name }}</h3>
+                        </a>
                     </div>
                 </div>
             @endforeach
