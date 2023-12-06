94935b15e51dd2e35c0752e64df172f9432df1f8
ken0727 <ayame0727@gmail.com>
2023-12-06 11:50:45 +0900

    ヘッダーちょっと修正

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,59 +6,59 @@
 </head>
 
 <header>
-
-        <div class="top-link">
-            <a href="/index/{{ Auth::user()->id }}">TOP</a>
-        </div>
-
-        <div class="top-link">
-            <a href="{{ route('posts.create') }}">投稿する</a>
-        </div>
-
-        <div class="top-link">
-            <a href="{{ route('bookmarks') }}">お気に入り投稿</a>
-            
-        </div>
+    <div class="top-link">
+        <a href="/index/{{ Auth::user()->id }}">TOP</a>
     </div>
 
-@auth
-        <div class="user-info">
-            <div class="dropdown">
-                <div id="user-icon-dropdown" class="user-icon">
-                    @if(auth()->user()->image_path)
-                        <img src="{{ asset('storage/' . auth()->user()->image_path) }}" alt="User Icon">
-                    @else
-                        <img src="{{ asset('storage/Noimage.jpeg') }}" alt="No Image">
-                    @endif
-                </div>
-                <div id="user-dropdown-content">
-                    <a href="{{ route('my-page.show') }}">{{ auth()->user()->name }}</a>
-                    <!-- 他のユーザー情報に関連するリンクなどもここに追加できます -->
-                    <a href="{{ route('logout') }}">ログアウト</a>
-                </div>
+    <div class="top-link">
+        <a href="{{ route('posts.create') }}">投稿する</a>
+    </div>
+
+    <div class="top-link">
+        <!-- ユーザーネームを表示 -->
+        {{ Auth::check() ? Auth::user()->name : '' }}
+    </div>
+
+    @auth
+    <div class="user-info">
+        <div class="dropdown">
+            <div id="user-icon-dropdown" class="user-icon">
+                @if(auth()->user()->image_path)
+                    <img src="{{ asset('storage/' . auth()->user()->image_path) }}" alt="User Icon">
+                @else
+                    <img src="{{ asset('storage/Noimage.jpeg') }}" alt="No Image">
+                @endif
+            </div>
+            <div id="user-dropdown-content">
+                <a href="{{ route('my-page.show') }}">マイページ</a>
+                <!-- 他のユーザー情報に関連するリンクなどもここに追加できます -->
+                <a href="{{ route('bookmarks') }}">お気に入り投稿</a>
+                <a href="{{ route('bookmarks') }}">お気に入りユーザー</a>
+                <a href="{{ route('logout') }}">ログアウト</a>
             </div>
         </div>
+    </div>
     @endauth
 
-<script>
-    $(document).ready(function () {
-        $("#user-icon-dropdown").click(function (event) {
-            event.stopPropagation();
-            // ドロップダウンの表示位置を調整
-            $("#user-dropdown-content").toggle().position({
-                my: "right top",
-                at: "right bottom",
-                of: this,
-                collision: "flipfit"
+    <script>
+        $(document).ready(function () {
+            $("#user-icon-dropdown").click(function (event) {
+                event.stopPropagation();
+                // ドロップダウンの表示位置を調整
+                $("#user-dropdown-content").toggle().position({
+                    my: "right top",
+                    at: "right bottom",
+                    of: this,
+                    collision: "flipfit"
+                });
+            });
+
+            $(document).on("click", function (event) {
+                if (!$(event.target).closest("#user-icon-dropdown").length) {
+                    $("#user-dropdown-content").hide();
+                }
             });
         });
-
-        $(document).on("click", function (event) {
-            if (!$(event.target).closest("#user-icon-dropdown").length) {
-                $("#user-dropdown-content").hide();
-            }
-        });
-    });
-</script>
+    </script>
 
 </header>
