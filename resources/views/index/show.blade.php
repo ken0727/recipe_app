50878bd9c072274aeb3130f6e78bd719b14a6244
ken0727 <ayame0727@gmail.com>
2023-12-06 11:14:57 +0900

    お気に入り投稿の左上に★を追加

--- resources/views/index/show.blade.php
+++ resources/views/index/show.blade.php
@@ -1,3 +1,5 @@
+
+
 @extends('layouts.app')
 
 @section('title', '投稿一覧')
@@ -12,19 +14,19 @@
                 @foreach ($chunk as $post)
                     <div class="col-md-4">
                         <div class="post-image-wrapper">
-                            <a href="{{ route('posts.show', $post) }}">
-                                @if ($post->image_path)
-                                    <img src="{{ asset('storage/' . $post->image_path) }}" alt="{{ $post->name }}" width="300" height="200">
-                                @else
-                                    <img src="{{ asset('storage/Noimage.jpeg') }}" alt="Noimage" width="300" height="200">
-                                @endif
-                                <h3>{{ $post->name }}</h3>
-                            </a>
+                        <a href="{{ route('posts.show', $post) }}" style="position: relative; display: inline-block;">
+                            @if ($post->image_path)
+                                <img src="{{ asset('storage/' . $post->image_path) }}" alt="{{ $post->name }}" width="300" height="200">
+                            @else
+                                <img src="{{ asset('storage/Noimage.jpeg') }}" alt="Noimage" width="300" height="200">
+                            @endif
                             @if (Auth::check() && Auth::user()->bookmarkRecipes->contains('post_id', $post->id))
                                 <!-- ★マークを表示 -->
-                                <span style="color: gold;">★</span>
+                                <span style="position: absolute; top: 0; left: 0; color: gold; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">★</span>
                             @endif
-                        </div>
+                            <h3>{{ $post->name }}</h3>
+                        </a>
+                    </div>
                     </div>
                 @endforeach
             </div>
